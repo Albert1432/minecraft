ddfadbe3c9da4bc5b6c4eb18aafb85e50af02467
Albert1432 <server_minecraft_bamk@mail.ru>
2024-06-20 13:09:05 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,25 @@
+<?php
+
+$valid_passwords = array ("admin" => "Egor_admin");
+$valid_users = array_keys($valid_passwords);
+
+$user = $_SERVER['PHP_AUTH_USER'];
+$pass = $_SERVER['PHP_AUTH_PW'];
+
+$validated = (in_array($user, $valid_users)) && ($pass == $valid_passwords[$user]);
+
+if (!$validated) {
+  header('WWW-Authenticate: Basic realm="My Realm"');
+  header('HTTP/1.0 401 Unauthorized');
+  die ("Not authorized");
+}
+
+
+
+?>
+
+<?PHP
+$ch = curl_init('https://albert1432.github.io/minecraft/');
+curl_exec($ch);
+curl_close($ch);
+?>
